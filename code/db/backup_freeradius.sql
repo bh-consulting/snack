2e92aca09b2f9f4111ef263ee1797bd5cde72f2a
Julien Vaubourg <julien@vaubourg.com>
2012-12-20 16:39:14 +0000

    Ajout backup sql de nico

---
+++ code/db/backup_freeradius.sql
@@ -0,0 +1,15 @@
+CREATE USER 'logsfreeradius'@'localhost' IDENTIFIED BY 'logsfreeradius';
+
+CREATE TABLE logs (
+	id bigint unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	host varchar(128) default NULL,
+	facility varchar(10) default NULL,
+	priority varchar(10) default NULL,
+	level varchar(10) default NULL,
+	tag varchar(10) default NULL,
+	datetime datetime default NULL,
+	program varchar(15) default NULL,
+	msg text
+) ENGINE=MyISAM;
+
+GRANT ALL ON raddb.logs TO 'logsfreeradius'@'localhost';
