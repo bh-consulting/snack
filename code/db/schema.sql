3088e924fb134363cf1b56cb03f510eacaa9dc30
Julien Guépin <julien@guepin.fr>
2013-01-07 15:28:08 +0000

    fixed bugs in db schema (only works when db not existing)

--- code/db/schema.sql
+++ code/db/schema.sql
@@ -12,14 +12,14 @@
 ###########################################################################
 
 CREATE DATABASE raddb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE user 'radius' identified by 'radiusroxx';
+CREATE user 'radius'@'localhost' identified by 'radiusroxx';
 
-GRANT ALL ON raddb to 'radius'@'localhost';
+GRANT ALL ON raddb.* to 'radius'@'localhost';
 
 #
 # Table structure for table 'radacct'
 #
-CREATE TABLE radacct (
+CREATE TABLE raddb.radacct (
   radacctid bigint(21) NOT NULL auto_increment,
   acctsessionid varchar(64) NOT NULL default '',
   acctuniqueid varchar(32) NOT NULL default '',
@@ -61,7 +61,7 @@ CREATE TABLE radacct (
 # Table structure for table 'radcheck'
 #
 
-CREATE TABLE radcheck (
+CREATE TABLE raddb.radcheck (
   id int(11) unsigned NOT NULL auto_increment,
   username varchar(64) NOT NULL default '',
   attribute varchar(64)  NOT NULL default '',
@@ -75,7 +75,7 @@ CREATE TABLE radcheck (
 # Table structure for table 'radgroupcheck'
 #
 
-CREATE TABLE radgroupcheck (
+CREATE TABLE raddb.radgroupcheck (
   id int(11) unsigned NOT NULL auto_increment,
   groupname varchar(64) NOT NULL default '',
   attribute varchar(64)  NOT NULL default '',
@@ -89,7 +89,7 @@ CREATE TABLE radgroupcheck (
 # Table structure for table 'radgroupreply'
 #
 
-CREATE TABLE radgroupreply (
+CREATE TABLE raddb.radgroupreply (
   id int(11) unsigned NOT NULL auto_increment,
   groupname varchar(64) NOT NULL default '',
   attribute varchar(64)  NOT NULL default '',
@@ -103,7 +103,7 @@ CREATE TABLE radgroupreply (
 # Table structure for table 'radreply'
 #
 
-CREATE TABLE radreply (
+CREATE TABLE raddb.radreply (
   id int(11) unsigned NOT NULL auto_increment,
   username varchar(64) NOT NULL default '',
   attribute varchar(64) NOT NULL default '',
@@ -118,7 +118,7 @@ CREATE TABLE radreply (
 # Table structure for table 'radusergroup'
 #
 
-CREATE TABLE radusergroup (
+CREATE TABLE raddb.radusergroup (
   username varchar(64) NOT NULL default '',
   groupname varchar(64) NOT NULL default '',
   priority int(11) NOT NULL default '1',
@@ -129,7 +129,7 @@ CREATE TABLE radusergroup (
 # Table structure for table 'radpostauth'
 #
 
-CREATE TABLE radpostauth (
+CREATE TABLE raddb.radpostauth (
   id int(11) NOT NULL auto_increment,
   username varchar(64) NOT NULL default '',
   pass varchar(64) NOT NULL default '',
@@ -141,7 +141,7 @@ CREATE TABLE radpostauth (
 #
 # Table structure for table 'nas'
 #
-CREATE TABLE nas (
+CREATE TABLE raddb.nas (
   id int(10) NOT NULL auto_increment,
   nasname varchar(128) NOT NULL,
   shortname varchar(32),
@@ -158,7 +158,7 @@ CREATE TABLE nas (
 #
 # Table structure for table 'raduser'
 #
-CREATE TABLE raduser (
+CREATE TABLE raddb.raduser (
     id int(11) unsigned NOT NULL auto_increment,
     username varchar(64) NOT NULL default '',
     admin boolean default '0',
@@ -175,7 +175,7 @@ CREATE TABLE raduser (
 #
 # Table structure for table 'radgroup'
 #
-CREATE TABLE radgroup (
+CREATE TABLE raddb.radgroup (
     id int(11) unsigned NOT NULL auto_increment,
     groupname varchar(64) NOT NULL default '',
     cert_path varchar(255),
