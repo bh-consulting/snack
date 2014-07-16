e8bb971ef44ea3a5b2ee4a17cb77668d3bc6829a
Guillaume Roche <guigeek@gmail.com>
2014-07-17 00:51:01 +0200

    Bug in scriptLogsRotate.sh where the file to archive isn't exist

--- tools/scriptLogsRotate.sh
+++ tools/scriptLogsRotate.sh
@@ -8,5 +8,7 @@ HOMESNACK=/home/snack
 mv $HOMESNACK/logs/snacklog $HOMESNACK/logs/snacklog-$WEEKN
 service syslog-ng restart
 WEEKTOARCHIVE=$((WEEKN-4))
-cd $HOMESNACK/logs && tar jcvf snacklog-$WEEKTOARCHIVE.tar.bz2 snacklog-$WEEKTOARCHIVE
-rm $HOMESNACK/logs/snacklog-$WEEKTOARCHIVE
+if [ -f $HOMESNACK/logs/snacklog-$WEEKTOARCHIVE ]; then
+    cd $HOMESNACK/logs && tar jcvf snacklog-$WEEKTOARCHIVE.tar.bz2 snacklog-$WEEKTOARCHIVE
+    rm $HOMESNACK/logs/snacklog-$WEEKTOARCHIVE
+fi
