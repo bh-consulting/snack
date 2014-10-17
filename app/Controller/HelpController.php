17d8fafff8c50aec1959c6199ccebfdd3c08e8a8
Guillaume Roche <groche@guigeek.org>
2014-10-18 00:47:51 +0200

    Ajout de l'aide pour la cle RSA

--- app/Controller/HelpController.php
+++ app/Controller/HelpController.php
@@ -1,7 +1,14 @@
 <?php
 class HelpController extends AppController {
     public function index() {
-       
+       $file = new File('/home/snack/id_rsa.pub', false, 0644);
+        $tmp="";
+        if ($file->exists()) {
+            $tmp=$file->read(false, 'rb', false);
+            if(preg_match('/^ssh-rsa .*/', $tmp, $matches)) {
+                $this->set('sshrsa', $tmp);
+            }
+        }
     }
     
     public function windows() {
