29e8f7b487850f942d62fd029465bc5b12bd6219
Guillaume Roche <groche@guigeek.org>
2014-10-30 23:24:08 +0100

    live sessions

--- app/Controller/RadacctsController.php
+++ app/Controller/RadacctsController.php
@@ -248,7 +248,155 @@ class RadacctsController extends AppController {
     }
     
     public function get_sessions_ajax() {
-        $this->index();
+        if ($this->request->is('post')) {
+            if (isset($this->request->data['MultiSelection']['sessions'])
+                && is_array($this->request->data['MultiSelection']['sessions'])
+            ) {
+                $success = true;
+                foreach( $this->request->data['MultiSelection']['sessions']
+                    as $sessionId ) {
+                        switch( $this->request->data['action'] ) {
+                        case "delete":
+                            $success = $success 
+                                && $this->Radacct->delete($sessionId);
+                            if ($success) {
+                                Utils::userlog(
+                                    __('deleted session %s',
+                                    $sessionId));
+                            } else {
+                                Utils::userlog(
+                                    __(
+                                        'error while deleting session %s',
+                                        $sessionId
+                                    ),
+                                    'error'
+                                );
+                            }
+                            break;
+                        }
+                    }
+
+                if ($success) {
+                    switch( $this->request->data['action'] ) {
+                    case "delete":
+                        $this->Session->setFlash(
+                            __('Sessions have been deleted.'),
+                            'flash_success'
+                        );
+                        Utils::userlog(
+                            __('deleted session %s', $sessionId)
+                        );
+                        break;
+                    }
+                } else {
+                    switch( $this->request->data['action'] ) {
+                    case "delete":
+                        $this->Session->setFlash(
+                            __('Unable to delete sessions.'),
+                            'flash_error'
+                        );
+                        break;
+                    }
+                }
+            } else {
+                $this->Session->setFlash(
+                    __('Please, select at least one session!'),
+                    'flash_warning'
+                );
+            }
+        }
+
+        $this->Filters->addDatesConstraint(array(
+            'fields' => array('acctstarttime', 'acctstoptime'), 
+            'from' => 'datefrom',
+            'to' => 'dateto',
+        ));
+
+        $this->Filters->addStringConstraint(array(
+            'fields' => array(
+                'acctuniqueid',
+                'username',
+                'callingstationid',
+                'nasipaddress',
+                'nasportid',
+            ),
+            'input' => 'text',
+            'ahead' => array('username','callingstationid', 'nasipaddress'),
+        ));
+
+        $this->Filters->addSelectConstraint(array(
+            'fields' => array('nasporttype'),
+            'data' => array('nasporttype'),
+            'translate' => $this->Radacct->types,
+            'input' => 'porttype',
+            'title' => __('Select a port type...'),
+        ));
+	
+        $this->Filters->addComplexConstraint(array(
+            'select' => array(
+                'items' => array(
+                    'active' => __(' '),
+                ),
+                'input' => 'active',
+                'title' => false,
+            ),
+            'callback' => array(
+                'getActive',
+                array(
+                    'input' => 'active',
+                ),
+            )
+        ));
+        
+        $sessions = $this->Filters->paginate();
+
+        $users = array();
+        $devices = array();
+
+        foreach ($sessions as &$session) {
+            $users[$session['Radacct']['radacctid']] =
+                $this->Users->extendUsers($session['Radacct']['username']);
+
+            $devices[$session['Radacct']['radacctid']] =
+                $this->Nas->extendNasByIP($session['Radacct']['nasipaddress']);
+
+            $session['Radacct']['duration'] = Utils::formatDate(
+                array(
+                    $session['Radacct']['acctstarttime'],
+                    $session['Radacct']['acctstoptime'],
+                ),
+                'durdisplay'
+            ); 
+
+	    if(is_null($session['Radacct']['acctstoptime'])) {
+		$session['Radacct']['durationsec'] = Utils::formatDate(
+		    array(
+			$session['Radacct']['acctstarttime'],
+			$session['Radacct']['acctstoptime'],
+		    ),
+		    'durdisplaysec'
+		); 
+	    } else
+		$session['Radacct']['durationsec'] = -1;
+
+            if (isset($session['Radacct']['acctstarttime'])) {
+                $session['Radacct']['acctstarttime'] = Utils::formatDate(
+                    $session['Radacct']['acctstarttime'],
+                    'display'
+                );
+            }
+
+            if (isset($session['Radacct']['acctstoptime'])) {
+                $session['Radacct']['acctstoptime'] = Utils::formatDate(
+                    $session['Radacct']['acctstoptime'],
+                    'display'
+                );
+            }
+        }
+        $this->set('users', $users);
+        $this->set('devices', $devices);
+	$this->set('types', $this->Radacct->types);
+        $this->set('radaccts', $sessions);
         $this->layout = false;
     }
 }
