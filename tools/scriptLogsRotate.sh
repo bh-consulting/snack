d33618f5847d94fda6c6d256a4f40a0db7c2b110
Guillaume Roche <groche@guigeek.org>
2015-04-05 02:00:36 +0200

    Fix bug in log rotate

--- tools/scriptLogsRotate.sh
+++ tools/scriptLogsRotate.sh
@@ -6,7 +6,7 @@ WEEKN=`date +%Y-%W`
 HOMESNACK=/home/snack
 
 mv $HOMESNACK/logs/snacklog $HOMESNACK/logs/snacklog-$WEEKN
-service syslog-ng restart
+service rsyslog restart
 #WEEKTOARCHIVE=$((WEEKN-4))
 #if [ -f $HOMESNACK/logs/snacklog-$WEEKTOARCHIVE ]; then
 #    cd $HOMESNACK/logs && tar jcvf snacklog-$WEEKTOARCHIVE.tar.bz2 snacklog-$WEEKTOARCHIVE
