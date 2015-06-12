85d4fc896d1f963d32849cdca568ebb06aeefb18
root <groche@guigeek.org>
2015-06-12 22:38:31 +0200

    Add md5/sha1/filesize for tftp files

--- app/Controller/TftpController.php
+++ app/Controller/TftpController.php
@@ -7,7 +7,9 @@ App::uses('AppController', 'Controller');
  * Controller to handle tftp.
  */
 class TftpController extends AppController {
-
+	public $components = array(
+        'Session',
+    );
 	public $tftppath="/home/snack/tftp";
 
 	public function index() {
@@ -30,17 +32,68 @@ class TftpController extends AppController {
 
 	public function upload_file() {
 		if ($this->request->isPost()) {
-			//debug($_FILES);
-            if (move_uploaded_file($_FILES['data']['tmp_name']['importFile']['file'], $this->tftppath.'/'.$_FILES['data']['name']['importFile']['file'])) {
+			if (count($_FILES) > 0) {
+	            if (move_uploaded_file($_FILES['data']['tmp_name']['importFile']['file'], $this->tftppath.'/'.$_FILES['data']['name']['importFile']['file'])) {
+					$this->Session->setFlash(
+                        "Success", 'flash_success'
+                	);
+				}
+			} else {
+				$this->Session->setFlash(
+                        "Error", 'flash_error'
+                );
 			}
 		}
-		return $this->redirect(array('action' => 'index'));
+		$this->redirect(array('action' => 'index'), null, true);
 	}
 
 	public function list_files() {
+		$files_arr = array();
 		$dir = new Folder($this->tftppath);
 		$files = $dir->find('.*');
         sort($files);
-        return $files;
+        foreach($files as $filename) {
+        	$file_arr = array();
+        	$file_arr['filename'] = $filename;
+        	$file = new File($this->tftppath.'/'.$filename, false, 0644);
+        	$file_arr['md5'] = $file->md5(true);
+        	$file_arr['size'] = $this->bytesToSize($file->size());
+        	$file_arr['sha1'] = sha1_file($this->tftppath.'/'.$filename);
+        	$files_arr[] = $file_arr;
+        }
+        //debug($files_arr);
+        return $files_arr;
 	}
+
+	/**
+	 * Convert bytes to human readable format
+	 *
+	 * @param integer bytes Size in bytes to convert
+	 * @return string
+	 */
+	function bytesToSize($bytes, $precision = 2)
+	{  
+	    $kilobyte = 1024;
+	    $megabyte = $kilobyte * 1024;
+	    $gigabyte = $megabyte * 1024;
+	    $terabyte = $gigabyte * 1024;
+	   
+	    if (($bytes >= 0) && ($bytes < $kilobyte)) {
+	        return $bytes . ' B';
+	 
+	    } elseif (($bytes >= $kilobyte) && ($bytes < $megabyte)) {
+	        return round($bytes / $kilobyte, $precision) . ' KB';
+	 
+	    } elseif (($bytes >= $megabyte) && ($bytes < $gigabyte)) {
+	        return round($bytes / $megabyte, $precision) . ' MB';
+	 
+	    } elseif (($bytes >= $gigabyte) && ($bytes < $terabyte)) {
+	        return round($bytes / $gigabyte, $precision) . ' GB';
+	 
+	    } elseif ($bytes >= $terabyte) {
+	        return round($bytes / $terabyte, $precision) . ' TB';
+	    } else {
+	        return $bytes . ' B';
+	    }
+}
 }
--- app/View/Tftp/index.ctp
+++ app/View/Tftp/index.ctp
@@ -1,11 +1,13 @@
 
 <?php
 $this->assign('tftp_active', 'active');
+echo $this->Session->flash();
 ?>
 <div class="col-md-12">
 <h1>TFTP</h1>
-<div class="col-md-offset-2 col-md-6">
+<div class="col-md-offset-1 col-md-10">
 <?php
+
 echo $this->Html->link('<i class="glyphicon glyphicon-open glyphicon-white"></i> ' . __('Upload a file'), '#confirmupload',
     array(
         'class' => 'btn btn-success btn-large',
@@ -37,16 +39,20 @@ echo '</div>';
 ?>
 <p></p>
 <table class="table table-hover table-stripped table-condensed">
+<th></th><th>Filename</th><th>Size</th><th>MD5</th><th>SHA1</th><th></th>
 <?php
 	foreach ($files as $id => $file)  {
 		echo '<tr>';
 
-		echo '<td class="col-md-1"><span class="glyphicon glyphicon-file" aria-hidden="true"></span></td>';
+		echo '<td><span class="glyphicon glyphicon-file" aria-hidden="true"></span></td>';
 		echo '<td>'.$this->Html->link(
-		    $file,
+		    $file['filename'],
 		    array('controller' => 'tftp', 'action' => 'get_file/'.$id),
 		    array('escape' => false)
 		).'</td>';
+        echo '<td>'.$file['size'].'</td>';
+        echo '<td>'.$file['md5'].'</td>';
+        echo '<td>'.$file['sha1'].'</td>';
 		echo '<td class="col-md-1">'.$this->Html->link(
 		    '<span class="glyphicon glyphicon-trash" aria-hidden="true">',
 		    array('controller' => 'tftp', 'action' => 'delete_file/'.$id),
